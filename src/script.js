8531e9ae4c87eee7e789051d8ff7535cd89c5c89
cawzal <57325746+cawzal@users.noreply.github.com>
2019-11-12 12:13:32 +1300

    Drag lists

--- src/script.js
+++ src/script.js
@@ -1,10 +1,21 @@
 const newList = document.querySelector('.new-list');
 const addList = document.querySelector('.add');
 const inputList = document.querySelector('input');
+let moveTarget = null;
+let dropTarget = null;
+let begin = null;
+let moving = false;
+
+const placeholderList = document.createElement('div');
+placeholderList.className = 'placeholder-list';
 
 addList.addEventListener('click', (event) => {
+	const listContainer = document.createElement('div');
+	listContainer.className = 'list-container';
+
 	const listDiv = document.createElement('div');
 	listDiv.className = 'list';
+	listContainer.appendChild(listDiv);
 
 	const titleDiv = document.createElement('div');
 	titleDiv.className = 'title';
@@ -31,9 +42,45 @@ addList.addEventListener('click', (event) => {
 
 		newItem.insertAdjacentElement('beforebegin', itemDiv);
 	});
-	runFunction(addItem, 'click', 5);
 
-	newList.insertAdjacentElement('beforebegin', listDiv);
+	listDiv.addEventListener('mousedown', (event) => {
+		moveTarget = listDiv;
+		dropTarget = listDiv.parentNode;
+		begin = {
+			mX: event.clientX,
+			mY: event.clientY,
+			bX: moveTarget.parentNode.offsetLeft,
+			bY: moveTarget.parentNode.offsetTop,
+		};
+	});
+
+	runFunction(addItem, 'click', 15);
+
+	newList.insertAdjacentElement('beforebegin', listContainer);
+});
+
+document.addEventListener('mousemove', (event) => {
+	if (moveTarget == null)
+		return;
+	if (!moving) {
+		moving = true;
+		moveTarget.parentNode.replaceChild(placeholderList, moveTarget);
+		moveTarget.style.position = 'absolute';
+		moveTarget.style.zIndex = 3;
+		document.body.appendChild(moveTarget);
+	}
+
+	moveTarget.style.top = `${begin.bY - (begin.mY - event.clientY)}px`;
+	moveTarget.style.left = `${begin.bX - (begin.mX - event.clientX)}px`;
+});
+
+document.addEventListener('mouseup', (event) => {
+	dropTarget.replaceChild(moveTarget, placeholderList);
+	moveTarget.style.left = '0px';
+	moveTarget.style.top = '0px';
+	moveTarget = null;
+	begin = null;
+	moving = false;
 });
 
 function runFunction(el, fn, num) {
