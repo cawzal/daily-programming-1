7aeb3ba706ffeb632a7e06836136fe0acbe6992c
cawzal <57325746+cawzal@users.noreply.github.com>
2019-11-14 06:48:18 +1300

    Swap items partial implementation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-let counter = 0;
 const newList = document.querySelector('.new-list');
 const addList = document.querySelector('.add');
 const inputList = document.querySelector('input');
@@ -8,6 +7,10 @@ let dropTarget = null;
 let begin = null;
 let moving = false;
 const positions = [];
+let onItemDown = false;
+let moveItem = null;
+let dropItem = null;
+let itemPositions = [];
 
 const placeholderList = document.createElement('div');
 placeholderList.className = 'placeholder-list';
@@ -22,9 +25,8 @@ addList.addEventListener('click', (event) => {
 
 	const titleDiv = document.createElement('div');
 	titleDiv.className = 'title';
-	titleDiv.textContent = inputList.value || `Default ${counter}`;
+	titleDiv.textContent = inputList.value || `Default ${getId()}`;
 	listDiv.appendChild(titleDiv);
-
 
 	const newItem = newList.cloneNode(true);
 	listDiv.appendChild(newItem);
@@ -36,9 +38,22 @@ addList.addEventListener('click', (event) => {
 		itemContainer.className = 'item-container';
 
 		const itemDiv = document.createElement('div');
-		itemDiv.textContent = inputItem.value || 'Default';
+		itemDiv.textContent = inputItem.value || `Default ${getId()}`;
 		itemDiv.className = 'item';
 		itemContainer.appendChild(itemDiv);
+
+		itemDiv.addEventListener('mousedown', (event) => {
+			console.log('Item mouse down!'); // @_@
+			onItemDown = true;
+			moveItem = itemDiv;
+			dropItem = itemDiv.parentNode;
+			begin = {
+				mX: event.clientX,
+				mY: event.clientY,
+				bX: moveItem.parentNode.offsetLeft,
+				bY: moveItem.parentNode.offsetTop,
+			};
+		});
 
 		const removeButton = document.createElement('button');
 		removeButton.textContent = 'X';
@@ -52,6 +67,9 @@ addList.addEventListener('click', (event) => {
 	});
 
 	listDiv.addEventListener('mousedown', (event) => {
+		if (onItemDown)
+			return;
+		console.log('Listmouse down!'); // @_@
 		moveTarget = listDiv;
 		dropTarget = listDiv.parentNode;
 		begin = {
@@ -70,51 +88,113 @@ addList.addEventListener('click', (event) => {
 		y: listContainer.offsetTop,
 		ref: listContainer
 	});
-	counter++;
 });
 
 document.addEventListener('mousemove', (event) => {
-	if (moveTarget == null)
-		return;
-	if (!moving) {
-		moving = true;
-		moveTarget.style.position = 'absolute';
-		moveTarget.style.zIndex = 3;
-		document.body.appendChild(moveTarget); // take off parent container
-	}
-	moveTarget.style.top = `${begin.bY - (begin.mY - event.clientY)}px`;
-	moveTarget.style.left = `${begin.bX - (begin.mX - event.clientX)}px`;
+	event.preventDefault();
+	if (moveTarget != null) {
+		if (!moving) {
+			moving = true;
+			moveTarget.style.position = 'absolute';
+			moveTarget.style.zIndex = 3;
+			document.body.appendChild(moveTarget); // take off parent container
+		} else {
+			moveTarget.style.top = `${begin.bY - (begin.mY - event.clientY)}px`;
+			moveTarget.style.left = `${begin.bX - (begin.mX - event.clientX)}px`;
 
-	// assign new drop target?
-	const mX = event.clientX;
-	const mY = event.clientY;
-	let target = null;
-	for (let i = 0; i < positions.length; i++) {
-		const t = positions[i];
-		if (mX < t.x + 200 && mX > t.x) {
-			target = t.ref;
-			break;
+			// assign new drop target?
+			const mX = event.clientX;
+			const mY = event.clientY;
+			let target = null;
+			for (let i = 0; i < positions.length; i++) {
+				const t = positions[i];
+				if (mX < t.x + 200 && mX > t.x) {
+					target = t.ref;
+					break;
+				}
+			}
+			if (target === dropTarget || target == null)
+				return;
+
+			// moved over another container
+			// move that list to the current drop target and update drop target
+			const child = target.firstElementChild;
+			target.removeChild(child);
+			dropTarget.appendChild(child);
+			dropTarget = target;
 		}
 	}
-	if (target === dropTarget || target == null)
-		return;
 
-	// moved over another container
-	// move that list to the current drop target and update drop target
-	const child = target.firstElementChild;
-	target.removeChild(child);
-	dropTarget.appendChild(child);
-	dropTarget = target;
+	// started to move an item
+	if (dropItem) {
+		if (!moving) {
+			itemPositions = [];
+			[...dropItem.parentNode.querySelectorAll('.item-container')].forEach((el) => {
+				itemPositions.push({
+					x: el.offsetLeft,
+					y: el.offsetTop,
+					ref: el
+				});
+			});
+
+			const parent = moveItem.parentNode;
+			const  { offsetHeight } = parent;
+			parent.style.height = `${offsetHeight}px`;
+			moving = true;
+
+			const { offsetWidth } = moveItem;
+			moveItem.style.position = 'absolute';
+			moveItem.style.zIndex = 3;
+			moveItem.style.width = `${offsetWidth}px`;
+			document.body.appendChild(moveItem); // take off parent container
+		} else {
+			moveItem.style.top = `${begin.bY - (begin.mY - event.clientY)}px`;
+			moveItem.style.left = `${begin.bX - (begin.mX - event.clientX)}px`;
+
+			const mX = event.clientX;
+			const mY = event.clientY;
+			let target = null;
+			for (let i = 0; i < itemPositions.length; i++) {
+				const t = itemPositions[i];
+				if (mY < t.y + 25 && mY > t.y) {
+					target = t.ref;
+					break;
+				}
+			}
+			if (target === dropItem || target == null)
+				return;
+			dropItem.style.height = '';
+			const child = target.firstElementChild;
+			target.style.height = `${moveItem.offsetHeight}px`;
+			target.removeChild(child);
+			dropItem.appendChild(child);
+			dropItem = target;
+		}
+	}
 });
 
 document.addEventListener('mouseup', (event) => {
-	dropTarget.appendChild(moveTarget);
-	moveTarget.style.left = '0px';
-	moveTarget.style.top = '0px';
-	moveTarget = null;
-	dropTarget = null;
-	begin = null;
-	moving = false;
+	if (dropTarget) {
+		dropTarget.appendChild(moveTarget);
+		moveTarget.style.left = '0px';
+		moveTarget.style.top = '0px';
+		moveTarget = null;
+		dropTarget = null;
+		begin = null;
+		moving = false;
+	}
+	if (dropItem) {
+		dropItem.appendChild(moveItem);
+		dropItem.style.height = '';
+		moveItem.style.left = '0px';
+		moveItem.style.top = '0px';
+		moveItem.style.position = 'relative';
+		moveItem = null;
+		dropItem = null;
+		begin = null;
+		moving = false;
+		onItemDown = false;
+	}
 });
 
 function runFunction(el, fn, num) {
@@ -122,5 +202,13 @@ function runFunction(el, fn, num) {
 		el[fn]();
 	}
 }
+
+function generateId() {
+	let counter = 0;
+	return function () {
+		return counter++;
+	}
+}
+const getId = generateId();
 
 runFunction(addList, 'click', 5);
